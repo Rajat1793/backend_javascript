0eb8a8b2409b9fcd3b4340d27ee2406256b79c1e
Rajat Jaiswal <rajaiswal@juniper.net>
2024-11-21 17:11:39 +0530

    utils

--- src/index.js
+++ src/index.js
@@ -8,6 +8,19 @@ dotenv.config({
     path: "./env"
 })
 connectDB()
+.then(() => {
+    app.on("error", (error) =>{
+        console.error("Error: ", error);
+        throw error
+    })
+    app.listen(process.env.PORT || 8000, () =>{
+        console.log(`Server is running on port: ${process.env.PORT}`);
+    })
+}
+)
+.catch((error) =>{
+    console.log("MongoDB connection failed!!", error);
+})
 
 // ANOTHER WAY TO CONNECT TO DB
 // const app = express()
---
+++ src/utils/asyncHandler.js
@@ -0,0 +1,24 @@
+// we are passing function as a argument which will be further passed to try catch in async format
+// using async and await
+// const asyncHandler = (func) => async(req, res, next) => {
+//     try {
+//         await func(res, req, next)
+//     } catch (error) {
+//         res.status(error.code || 500).json({
+//             success: false,
+//             message: error.message
+//         })
+//     }
+// }
+
+// export {asyncHandler}
+
+// using promises
+const asynchandler = (requestHandler) => {
+    (req, res, next) =>{
+        Promise.resolve(requestHandler(req, res, next)).
+        catch((error) => next(error))
+    }
+}
+
+export {asynchandler}
